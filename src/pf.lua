336f3822d052342e51399f06e01d6bc859f2b8ce
Andy Wingo <wingo@igalia.com>
2014-06-24 09:30:05 +0200

    Fold pcap.lua into pf.lua
    * src/pf.lua: Fold pcap.lua into pf.lua, as we need to test these
      together.
    
    * src/Makefile: Update.

--- src/pf.lua
+++ src/pf.lua
@@ -3,6 +3,7 @@ module("pf",package.seeall)
 local ffi = require("ffi")
 local bit = require("bit")
 local band = bit.band
+local pcap -- The pcap library, lazily loaded.
 
 -- Note: the bit module represents uint32_t values with the high-bit set
 -- as negative int32_t values, so we do the same for all of our 32-bit
@@ -11,6 +12,13 @@ local band = bit.band
 ffi.cdef[[
 struct bpf_insn { uint16_t code; uint8_t jt, jf; int32_t k; };
 struct bpf_program { uint32_t bf_len; struct bpf_insn *bf_insns; };
+
+typedef struct pcap pcap_t;
+int pcap_datalink_name_to_val(const char *name);
+pcap_t *pcap_open_dead(int linktype, int snaplen);
+void pcap_perror(pcap_t *p, const char *suffix);
+int pcap_compile(pcap_t *p, struct bpf_program *fp, const char *str,
+                 int optimize, uint32_t netmask);
 ]]
 local bpf_program_mt = {
   __len = function (program) return program.bf_len end,
@@ -263,7 +271,7 @@ local function compile_bpf_prog (instructions)
       end
    end
 
-   for i=0, #instructions do
+   for i=0, #instructions-1 do
       local inst = instructions[i]
       local code = inst.code
       local class = BPF_CLASS(code)
@@ -283,7 +291,50 @@ local function compile_bpf_prog (instructions)
    return 'function ()\n' .. head .. body .. '  error ("end of bpf?")\nend'
 end
 
+-- The dlt_name is a "datalink type name" and specifies the link-level
+-- wrapping to expect.  E.g., for raw ethernet frames, you would specify
+-- "EN10MB" (even though you have a 10G card), which corresponds to the
+-- numeric DLT_EN10MB value from pcap/bpf.h.  See
+-- http://www.tcpdump.org/linktypes.html for more details on possible
+-- names.
+--
+-- You probably want "RAW" for raw IP (v4 or v6) frames.  If you don't
+-- supply a dlt_name, "RAW" is the default.
+function pcap_compile (filter_str, dlt_name)
+   if not pcap then pcap = ffi.load("pcap") end
+
+   dlt_name = dlt_name or "RAW"
+   local dlt = pcap.pcap_datalink_name_to_val(dlt_name)
+   assert(dlt >= 0, "bad datalink type name " .. dlt_name)
+   local snaplen = 65535 -- Maximum packet size.
+   local p = pcap.pcap_open_dead(dlt, snaplen)
+
+   assert(p, "pcap_open_dead failed")
+
+   -- pcap_compile
+   local program = pf.bpf_program()
+   local err = pcap.pcap_compile(p, program, filter_str, 0, 0)
+
+   if err ~= 0 then
+      pcap.pcap_perror(p, "pcap_compile failed!")
+      error("pcap_compile failed")
+   end
+
+   return program
+end
+
+function dump_bytecode (prog)
+   io.write(#prog .. ':\n')
+   for i = 0, #prog-1 do
+      io.write(string.format('  {0x%x, %u, %u, %d}\n',
+                             prog[i].code, prog[i].jt, prog[i].jf, prog[i].k))
+   end
+   io.write("\n")
+end
+
 function selftest ()
    print("selftest: pf")
+   prog = pcap_compile("icmp")
+   dump_bytecode(prog)
    print("OK")
 end
